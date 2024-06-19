a67fcf3c939a7d706829bd4a1038de5f6404def5
Konnor Rogers <konnor5456@gmail.com>
2024-06-19 15:53:47 -0400

    fix snapshots

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
+// This kind of hacky,but it "fixes" snapshots
+import ShoelaceButtonElement from "@shoelace-style/shoelace/dist/components/button/button.component.js"
+customElements.define("sl-button", ShoelaceButtonElement)
+
 const SlButton = dynamic(
-  () => import("@shoelace-style/shoelace/dist/react/button/index.js"),
+  () => import("@shoelace-style/shoelace/cdn/react/button/index.js"),
   {
     loading: () => <p>Loading...</p>,
     ssr: false,
